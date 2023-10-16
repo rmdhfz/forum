16b5701f950947542c6735fee2351010e08c1dd2
Hafiz Ramadhan <hfzrmd@gmail.com>
2023-10-16 22:06:31 +0700

    update: content: newest content

--- app/controllers/api/Content.php
+++ app/controllers/api/Content.php
@@ -45,4 +45,32 @@ class Content extends RestController {
         ");
         success('success', $data->result());
     }
+
+    function newtopics_get()
+    {
+        $this->load->database();
+        $data = $this->db->query("
+            SELECT
+            p.id,
+            cp.name as category,
+            p.title,
+            p.views,
+            SUBSTRING(p.content, 1, 50) as content,
+            p.created_at,
+            p.created_by
+
+            FROM
+                post p
+            INNER JOIN category cp ON p.category_id = cp.id
+            LEFT JOIN comment c ON c.post_id = p.id
+
+            WHERE
+                p.is_deleted IS NULL AND
+                cp.is_deleted IS NULL AND
+                c.is_deleted IS NULL AND
+                p.is_publish = ? AND
+                cp.name = ? GROUP BY p.id LIMIT 3
+        ", [1, "diskusi"]);
+        success('success', $data->result());
+    }
 }
